14e2d01c0fb6dfb6f4369ea8b38c0d55bc5025ff
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-29 14:57:09 +0100

    chore: Register NoteModule in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { HighlightModule } from './highllight/highlight.module';
+import { NoteModule } from './note/note.module';
 import { PrismaService } from './prisma.service';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), BookModule, SharedModule, HighlightModule],
+  imports: [
+    ConfigModule.forRoot(),
+    BookModule,
+    SharedModule,
+    HighlightModule,
+    NoteModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
