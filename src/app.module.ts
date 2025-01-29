69d46da7338380bf9a463d317de463ffea216ecf
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-29 14:57:53 +0100

    feat: Add Highlight module with core functionality
    - Created HighlightController with endpoints for retrieving, saving, and favoriting highlights
    - Implemented HighlightService with methods for processing and saving highlights
    - Added HighlightModule with necessary dependencies and imports
    - Fixed import path for HighlightModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
-import { HighlightModule } from './highllight/highlight.module';
+import { HighlightModule } from './highlight/highlight.module';
 import { NoteModule } from './note/note.module';
 import { PrismaService } from './prisma.service';
 import { SharedModule } from './shared/shared.module';
