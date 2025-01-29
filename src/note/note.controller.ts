7b3a9aab825a635faa738ad35fd4ed8a7d41a87d
Quentin-Lecocq <quentin.lecocq@yahoo.com>
2025-01-29 16:40:33 +0100

    feat: Implement Note creation endpoint with highlight association
    - Updated NoteController to handle note creation for a specific highlight
    - Added createNote method with proper request handling and error management
    - Configured NoteService to create notes using Prisma
    - Adjusted ESLint configuration to relax some TypeScript type safety rules

--- src/note/note.controller.ts
+++ src/note/note.controller.ts
@@ -1,10 +1,40 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { NoteService } from './note.service';
 
-@Controller('notes')
+@Controller('api/notes')
 export class NoteController {
   constructor(private readonly noteService: NoteService) {}
 
   @Post('highlight/:id')
-  async createNote() {}
+  @HttpCode(HttpStatus.CREATED)
+  async createNote(
+    @Param('id') id: string,
+    @Body()
+    postData: {
+      content: string;
+    },
+  ): Promise<{ status: number }> {
+    try {
+      await this.noteService.createNote({
+        content: postData.content,
+        highlight: {
+          connect: { id },
+        },
+      });
+
+      return {
+        status: HttpStatus.CREATED,
+      };
+    } catch (error: unknown) {
+      console.error('Error create note', error);
+      return { status: HttpStatus.INTERNAL_SERVER_ERROR };
+    }
+  }
 }
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Note, Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class NoteService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createNote(data: Prisma.NoteCreateInput): Promise<Note> {
+    return this.prisma.note.create({
+      data,
+    });
+  }
 }
